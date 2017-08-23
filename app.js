02df18d9185027a315535fd7c9f47292683c55de
Taumich <michael.arenander@gmail.com>
2017-08-23 21:52:18 +0200

    list + values can now be displayed
    understood how to used angular to reach data from database

--- app.js
+++ app.js
@@ -14,14 +14,6 @@ var MainController = function($scope){
 
 var app = angular.module('login', []);
 app.controller('login-ctrl', function($scope){
-    $scope.message = "wazappa";
-    $scope.hej = false;
-    $scope.actionLink = 'refugee-request.html';
-    $scope.setAction = function(){
-        if( $scope.actionLink == 'refugee-request.html'){
-            $scope.actionLink = 'volunteer-map.html';
-        }else{
-            $scope.actionLink = 'refugee-request.html';
-        }
-    }
-})
+    $scope.message = player;
+    
+});
---
+++ database.js
@@ -0,0 +1,20 @@
+var wasap = "wasap";
+
+var player =
+    {
+        player1: {
+            id:"player01",
+            name:"wrecker",
+            score: 500
+        },
+        player2: {
+            id:"player02",
+            name:"squack",
+            score: 100
+        },
+        player3: {
+            id:"player03",
+            name:"wayne",
+            score: 120
+        }
+    };
